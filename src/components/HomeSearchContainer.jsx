c580276e678da1c2cfb29919b12310d277582d1f
BashithR <bkratnaweera04@gmail.com>
2025-12-22 21:20:40 +0530

    Finished styling for The Home Seach Bar

--- src/components/HomeSearchContainer.jsx
+++ src/components/HomeSearchContainer.jsx
@@ -1,13 +1,35 @@
 import "./HomeSearchContainer.css";
-
-import HomePNG from "../assets/Home.png";
+import SearchIcon from "../assets/searchicon.png";
 
 function HomeSearchContainer() {
   return (
     <>
       <div className="homeSearchContainer">
         <div className="container">
-          <img src={HomePNG} alt="Home Image" className="homeImage" />
+          <div className="innerContainer">
+            <p className="innerText">
+              <span className="specialText">believe</span> in finding it with
+              the <br></br>Sri Lanka's largest choice of homes
+            </p>
+            {/* Search Bar Stats from Here*/}
+            <div className="searchContainer">
+              <p id="BuyHeading">Buy</p>
+              <hr></hr>
+              <p>Search Properties to Buy</p>
+              <form id="searchForm">
+                <div id="searchBar">
+                  <img src={SearchIcon} alt="Search Icon"></img>
+                  <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="Flat, 3 bedrooms, BR6"
+                  ></input>
+                </div>
+                <button className="searchBtn">Search</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
