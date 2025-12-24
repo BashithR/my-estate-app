1b0dbcf2b3fd8d1fff5f8129abd600435ba440fa
BashithR <bkratnaweera04@gmail.com>
2025-12-24 12:10:45 +0530

    Added React Form widgets according to the coursework rubric

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
 import "./HomeSearch.css";
 import SearchIcon from "../assets/searchicon.png";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Button,
+  Heading,
+  Flex,
+  Image,
+} from "@chakra-ui/react";
 
 function HomeSearch({ onSearch }) {
   const [type, setType] = useState(""); // House, Flat, Any
@@ -39,60 +50,97 @@ function HomeSearch({ onSearch }) {
             <p>Search Properties to Buy</p>
             <form id="searchForm" onSubmit={handleSubmit}>
               {/* Property Type Dropdown */}{" "}
-              <select onChange={(e) => setType(e.target.value)} value={type}>
-                {" "}
-                <option value="">Property Type</option>{" "}
-                <option value="House">House</option>{" "}
-                <option value="Flat">Flat</option>{" "}
-                <option value="Any">Any</option>{" "}
-              </select>
+              <FormControl>
+                <FormLabel color="white">Property Type</FormLabel>
+                <Select
+                  placeholder="Select Property Type"
+                  bg="white"
+                  color="black"
+                  onChange={(e) => setType(e.target.value)}
+                  value={type}
+                >
+                  <option value="House">House</option>
+                  <option value="Flat">Flat</option>
+                </Select>
+              </FormControl>
               {/* Price Range Inputs */}
-              <input
-                type="number"
-                name="minPrice"
-                value={minPrice}
-                onChange={(e) => setMinPrice(e.target.value)}
-                placeholder="Min Price"
-              />
-              <input
-                type="number"
-                name="maxPrice"
-                value={maxPrice}
-                onChange={(e) => setMaxPrice(e.target.value)}
-                placeholder="Max Price"
-              />
+              <FormControl mt={{ base: 4, md: 0 }}>
+                <FormLabel color="white">Min Price</FormLabel>
+                <Input
+                  type="number"
+                  bg="white"
+                  color="black"
+                  value={minPrice}
+                  onChange={(e) => setMinPrice(e.target.value)}
+                  placeholder="Min Price"
+                />
+              </FormControl>
+              <FormControl mt={{ base: 4, md: 0 }}>
+                <FormLabel color="white">Max Price</FormLabel>
+                <Input
+                  type="number"
+                  bg="white"
+                  color="black"
+                  onChange={(e) => setMaxPrice(e.target.value)}
+                  placeholder="Max Price"
+                />
+              </FormControl>
               {/* Bedrooms Range Inputs */}
-              <input
-                type="number"
-                name="minBedrooms"
-                value={minBedrooms}
-                onChange={(e) => setMinBedrooms(e.target.value)}
-                placeholder="Min Bedrooms"
-              />
-              <input
-                type="number"
-                name="maxBedrooms"
-                value={maxBedrooms}
-                onChange={(e) => setMaxBedrooms(e.target.value)}
-                placeholder="Max Bedrooms"
-              />
+              <FormControl>
+                <FormLabel color="white">Min Bedrooms</FormLabel>
+                <Input
+                  type="number"
+                  bg="white"
+                  color="black"
+                  value={minBedrooms}
+                  onChange={(e) => setMinBedrooms(e.target.value)}
+                  placeholder="Min Bedrooms"
+                />
+              </FormControl>
+              <FormControl mt={{ base: 4, md: 0 }}>
+                <FormLabel color="white">Max Bedrooms</FormLabel>
+                <Input
+                  type="number"
+                  bg="white"
+                  color="black"
+                  value={maxBedrooms}
+                  onChange={(e) => setMaxBedrooms(e.target.value)}
+                  placeholder="Max Bedrooms"
+                />
+              </FormControl>
               {/* Date Added */}
-              <input
-                type="date"
-                name="dateAdded"
-                value={dateAdded}
-                onChange={(e) => setDateAdded(e.target.value)}
-              />
+              <FormControl>
+                <FormLabel color="white">Min Date Added</FormLabel>
+                <Input
+                  type="date"
+                  bg="white"
+                  color="black"
+                  value={dateAdded}
+                  onChange={(e) => setDateAdded(e.target.value)}
+                  // Ensure the user cannot pick a future date
+                />
+              </FormControl>
               {/* Postcode Input */}
-              <input
-                type="text"
-                name="postcode"
-                value={postcode}
-                onChange={(e) => setPostcode(e.target.value)}
-                placeholder="Postcode"
-              />
+              <FormControl mt={{ base: 4, md: 0 }}>
+                <FormLabel color="white">Postcode</FormLabel>
+                <Input
+                  bg="white"
+                  color="black"
+                  name="postcode"
+                  value={postcode}
+                  onChange={(e) => setPostcode(e.target.value)}
+                  placeholder="Postcode"
+                />
+              </FormControl>
               {/* Submit Button */}
-              <button className="searchBtn">Search</button>
+              <Button
+                colorScheme="teal"
+                width="100%"
+                mt={{ base: 4, md: 0, lg: 8 }}
+                type="submit"
+              >
+                Search
+              </Button>
             </form>
           </div>
         </div>
