73b6f0b6ae38732c88d17df7739d49407657d0e6
BashithR <bkratnaweera04@gmail.com>
2025-12-24 23:35:49 +0530

    Added Styles and Tab List to the Property Gallery, and added a new field called "title" to properties

--- src/components/PropertyGallery.jsx
+++ src/components/PropertyGallery.jsx
@@ -1,33 +1,135 @@
+import React, { useState } from "react";
 import {
   Box,
-  Image,
+  Flex,
   Heading,
+  Image,
   Text,
+  SimpleGrid,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   Button,
-  Stack,
-  Badge,
 } from "@chakra-ui/react";
 
-function PropertyGallery({ property, setCurrentView }) {
+function PropertyPage({ property, setCurrentView }) {
+  // State to handle the selected image from the thumbnails
+  const [selectedImage, setSelectedImage] = useState(property.picture);
+
+  // Function to handle thumbnail click
+  const handleThumbnailClick = (image) => {
+    setSelectedImage(image);
+  };
+
   return (
     <>
-      <h1>
-        Hi this is <b>{property.id}</b>
-      </h1>
-      <h1>
-        Hi this is <b>{property.type}</b>
-      </h1>
-      <Button
-        as="a"
-        colorScheme="teal"
-        size="sm"
-        alignSelf="flex-start"
-        onClick={() => setCurrentView("Home")}
-      >
-        Go Back
-      </Button>
+      <Box maxW="7xl" mx="auto" px={4} py={6}>
+        <Flex
+          direction={{ base: "column", md: "row" }}
+          align="center"
+          justify="space-between"
+          mb={4}
+          gap={4}
+        >
+          <Heading
+            fontWeight="semibold"
+            fontSize={{ base: "20px", md: "24px", lg: "28px" }}
+          >
+            {property.title} for LKR {property.price}
+          </Heading>
+          <Button
+            as="a"
+            onClick={() => setCurrentView("Home")}
+            colorScheme="teal"
+            size="sm"
+          >
+            Go Back
+          </Button>
+        </Flex>
+
+        {/* Property Gallery */}
+        <Flex
+          direction={{ base: "column", md: "row" }}
+          mb={6}
+          align="center"
+          justify="center"
+        >
+          {/* Large Image */}
+          <Box flex={1} mb={{ base: 4, md: 0 }} textAlign="center">
+            <Image
+              src={selectedImage}
+              alt={property.type}
+              boxSize="100%"
+              borderRadius="md"
+            />
+          </Box>
+
+          {/* Thumbnails */}
+          <Box flex={0.75} ml={{ base: 0, md: 4 }}>
+            <SimpleGrid columns={{ base: 3, md: 4, lg: 3 }} spacing={4}>
+              {property.images.map((image, index) => (
+                <Box
+                  key={index}
+                  borderWidth="1px"
+                  borderRadius="md"
+                  overflow="hidden"
+                  cursor="pointer"
+                  onClick={() => handleThumbnailClick(image)}
+                >
+                  <Image
+                    src={image}
+                    alt={`Thumbnail ${index + 1}`}
+                    boxSize="100%"
+                  />
+                </Box>
+              ))}
+            </SimpleGrid>
+          </Box>
+        </Flex>
+
+        {/* Tabs for Description, Floor Plan, Google Map */}
+        <Tabs isFitted variant="enclosed" colorScheme="teal">
+          <TabList mb={4}>
+            <Tab _selected={{ bg: "teal.500", color: "white" }}>
+              Description
+            </Tab>
+            <Tab _selected={{ bg: "teal.500", color: "white" }}>Floor Plan</Tab>
+            <Tab _selected={{ bg: "teal.500", color: "white" }}>Google Map</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Heading size="md" mb={4}>
+                Property Description
+              </Heading>
+              <Text>{property.description}</Text>
+            </TabPanel>
+            <TabPanel>
+              <Heading size="md" mb={4}>
+                Floor Plan
+              </Heading>
+              <Image src={property.floorPlan} alt="Floor Plan" />
+            </TabPanel>
+            <TabPanel>
+              <Heading size="md" mb={4}>
+                Google Map
+              </Heading>
+              {/* Embed Google Map (Replace with actual Google Map Embed Code) */}
+              <iframe
+                width="100%"
+                height="450"
+                frameBorder="0"
+                style={{ border: 0 }}
+                src={`https://www.google.com/maps/embed/v1/place?q=${property.location}&key=YOUR_GOOGLE_MAPS_API_KEY`}
+                allowFullScreen
+              ></iframe>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
     </>
   );
 }
 
-export default PropertyGallery;
+export default PropertyPage;
